1002170393ca2e56e10f962f9808d1c129bd438d
Yanilda <synac1@users.noreply.github.com>
2020-02-03 12:39:31 -0500

    Practice with variables CMP167

---
+++ PracticeWeek2.java
@@ -0,0 +1,28 @@
+/**
+ * @author Yanilda
+ * created: 2/3/20
+ * description: Variable Practice
+ **/
+public class PracticeWeek2 {
+	public static void main(String[] args) {
+		//Defining Variables
+		double wallet = 10.35;
+		byte   digit  = 5;
+		int    num    = 3000;
+	
+		//Declaring Variables
+		String name1;
+		String name2;
+		String name3;
+		
+		//Assigning Values
+		name1= "Mary";
+		name2= "John";
+		name3= "Thessa";
+		
+		//Printing 
+		System.out.println(name1);	
+		System.out.println(name1+name3);//Expected: MaryThesa
+		
+	}
+}
